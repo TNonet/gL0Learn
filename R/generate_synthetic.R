0e275a4df3d52aef7e83d4b6db3b8f0641de1fa0
Tim Nonet <tim.nonet@gmail.com>
2021-11-03 16:35:35 -0500

    implement synthetic dataset generating methods

--- R/generate_synthetic.R
+++ R/generate_synthetic.R
@@ -15,16 +15,59 @@
 #' 
 #' @export
 gL0Learn.generate_synthetic <- function(n, p, model, normalize, seed = 1, ...){
+    if (!(normalize %in% list("precision", "covariance"))){
+        stop("normalize must be either 'precision' or  'covariance'")
+    }
+    
     if (model == "KR1"){
         args = as.list(match.call())
         if (is.null(args$rho)){
             stop("When model is 'KR1', an additional parameter rho is needed." )
         }
-        return(gL0Learn.generate_Toeplitz_correlation(n,
-                                                      p,
-                                                      rho=args$rho,
-                                                      normalize=normalize,
-                                                      seed=seed))
+        return(gL0Learn.generate_toeplitz(n,
+                                          p,
+                                          rho=args$rho,
+                                          normalize=normalize,
+                                          seed=seed))
+    } else if (model == "independent"){
+        return(gL0Learn.generate_independent(n,
+                                             p,
+                                             normalize=normalize,
+                                             seed=seed))
+    } else if (model == "constant"){
+        args = as.list(match.call())
+        if (is.null(args$rho)){
+            stop("When model is 'constant', an additional parameter rho is needed." )
+        }
+        return(gL0Learn.generate_constant(n,
+                                          p,
+                                          rho=args$rho,
+                                          normalize=normalize,
+                                          seed=seed))
+    } else if (model == "banded"){
+        args = as.list(match.call())
+        if (is.null(args$rho)){
+            stop("When model is 'banded', an additional parameter rho is needed." )
+        }
+        return(gL0Learn.generate_banded(n,
+                                        p,
+                                        rho=args$rho,
+                                        normalize=normalize,
+                                        seed=seed))
+    } else if (model == "regression"){
+        args = as.list(match.call())
+        if (is.null(args$k)){
+            stop("When model is 'banded', an additional parameter k is needed." )
+        }
+        if (is.null(args$val)){
+            stop("When model is 'banded', an additional parameter val is needed." )
+        }
+        return(gL0Learn.generate_banded(n,
+                                        p,
+                                        rho=args$rho,
+                                        normalize=normalize,
+                                        seed=seed))
+        
     } else {
         stop("model is not supported.")
     }
