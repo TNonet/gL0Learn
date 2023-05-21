72271ba8522f1ab60ba6558a77461f3d27df65bd
Tim Nonet <tim.nonet@gmail.com>
2023-05-21 17:29:22 -0500

    Tnonet/r ci (#10)
    * Update R.yml
    
    * Update R.yml
    
    * Update R.yml
    
    * Update R.yml
    
    * Fix some warnings
    
    * Update Makevars.win
    
    * fix dirs_check
    
    * Update Makevars.win
    
    * Update .pre-commit-config.yaml
    
    * Update gL0Learn-benchmark.Rmd

--- pypkg/src/gl0learn/src/src/include/utils.h
+++ pypkg/src/gl0learn/src/src/include/utils.h
@@ -15,13 +15,13 @@ std::vector<std::size_t> inline nth_largest_indices(
 
   auto it = x.begin();
   const auto it_end = x.end();
-  for (size_t i = 0; it != it_end; ++i, ++it) {
+  for (std::size_t i = 0; it != it_end; ++i, ++it) {
     q.push(std::pair<double, int>(*it, i));
   }
 
   std::vector<std::size_t> indices;
   indices.reserve(n);
-  for (int i = 0; i < n; ++i) {
+  for (std::size_t i = 0; i < n; ++i) {
     indices.push_back(q.top().second);
     q.pop();
   }
